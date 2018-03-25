89216c059042c1ee021a12c372b609c575d12041
t_takano <cerberus1974@gmail.com>
2018-03-25 22:49:59 +0900

    子に値を渡せる

---
+++ BlazorApp02/BlazorApp02.Shared/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp02.Shared
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+    }
+}
